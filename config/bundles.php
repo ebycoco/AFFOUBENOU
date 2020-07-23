e1ec21dff041faad38c9e340acf91ab4bc0bab54
EBYCHOCO <brouyaoeric7@gmail.com>
2020-07-23 13:45:08 +0000

    integration de bundle encore

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
